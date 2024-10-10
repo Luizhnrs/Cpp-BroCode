dd29cd17eb1d7bf539a7f6f03160efac5a41fd9e
Luizhnrs <Luizhnrsg@gmail.com>
2024-10-10 13:59:50 -0300

    Variables and data types

---
+++ variables.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main(){
+
+    //integers (simple numbers)
+    int days = 5;
+    int numbers = 22;
+    int age = 19;
+
+    //double(prices, decimals, etc)
+    double price = 10.2;
+    double radio = 98.1;
+    double temperature = 32.1;
+
+    //char (single character)
+    char initial = 'A';
+    char grade = 'B';
+    char C = 'C';
+
+    //booleans (true or false)
+    bool isActive = true;
+    bool isNotActive = false;
+
+    //String is a Word or a Phrase
+    std::string name = "Luiz";
+    std::string phrase = "I like pizza";
+    std::string games = "My favorite game is Red Dead Redemption 2";
+
+    std::cout << "Hello: " << name << std::endl;
+    std::cout << "Whats your favorite game? " << games;
+
+    return 0;
+}
