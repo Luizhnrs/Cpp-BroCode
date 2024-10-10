55e38eed6aa3f0b0cb96dbd1f7ad899238fb2f4c
Luizhnrs <Luizhnrsg@gmail.com>
2024-10-10 14:09:06 -0300

    Const

---
+++ const.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    //const is a not mutable variable
+    const int SPEED_OF_LIGHT = 29979258;
+    const double PI = 3.14;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+
+    
+
+    return 0;
+}
