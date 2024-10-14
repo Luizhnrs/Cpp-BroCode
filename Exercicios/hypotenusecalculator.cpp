1bc07f45071bcffacd8e74ef97ccf0e8c4b7dc10
Luizhnrs <Luizhnrsg@gmail.com>
2024-10-14 16:40:37 -0300

    hypotenuse calculator

---
+++ Exercicios/hypotenusecalculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+    double a;
+    double b;
+    double c;   
+
+    a = 3;
+    b = 4;
+
+    c = sqrt(pow(a, 2) + pow(b, 2));
+
+    std::cout << c;
+    return 0;
+}
